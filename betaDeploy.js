fc74162f48c723f2794f6457b08f5e85c749c2bb
sahilalam0 <sahil.mansoor@anywhere.co>
2023-07-12 14:44:13 +0530

    Update betaDeploy.js

--- betaDeploy.js
+++ betaDeploy.js
@@ -2,7 +2,7 @@ module.exports =async ({context,exec,github})=>{
   const currentPRContainsBeta=context.payload.pull_request.labels.find((label)=>label.name==='beta');
   if(currentPRContainsBeta){
     console.log('Current PR contains Beta tag');
-    return;
+    return false;
   }
   console.log('Current PR does not contains Beta tag');
   await exec.exec('npm -v');
@@ -13,5 +13,6 @@ module.exports =async ({context,exec,github})=>{
   });
   const containsBeta=!!result.data.find((pr)=> !!pr.labels.find((label)=>label.name==='beta'));
   console.log('Any Open PR contains Beta : ', containsBeta);
-  await exec.exec('git branch --show-current')        
+  await exec.exec('git branch --show-current');
+  return true;
 }
